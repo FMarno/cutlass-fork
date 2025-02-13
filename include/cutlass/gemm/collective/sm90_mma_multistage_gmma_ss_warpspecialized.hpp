c680e801c061f8aede98baca50972a8bd7e75a18
Finlay Marno <finlay.marno@codeplay.com>
2025-02-13 16:49:55 +0000

    compiler workarounds

--- include/cutlass/gemm/collective/sm90_mma_multistage_gmma_ss_warpspecialized.hpp
+++ include/cutlass/gemm/collective/sm90_mma_multistage_gmma_ss_warpspecialized.hpp
@@ -289,7 +289,11 @@ struct CollectiveMma<
       --k_tile_count;
 
       // UNLOCK smem_pipe_write
+#if defined(CUTLASS_ENABLE_SYCL)
+      pipeline.producer_commit<cutlass::arch::cpasync_barrier_arrive>(smem_pipe_write);
+#else
       pipeline.producer_commit(smem_pipe_write, cutlass::arch::cpasync_barrier_arrive);
+#endif
 
       // Advance smem_pipe_write
       ++smem_pipe_write;
@@ -308,7 +312,11 @@ struct CollectiveMma<
       ++k_tile_iter;
 
       // UNLOCK smem_pipe_write
+#if defined(CUTLASS_ENABLE_SYCL)
+      pipeline.producer_commit<cutlass::arch::cpasync_barrier_arrive>(smem_pipe_write);
+#else
       pipeline.producer_commit(smem_pipe_write, cutlass::arch::cpasync_barrier_arrive);
+#endif
 
       // Advance smem_pipe_write
       ++smem_pipe_write;
