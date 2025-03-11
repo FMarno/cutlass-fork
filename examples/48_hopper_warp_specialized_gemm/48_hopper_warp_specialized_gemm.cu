36467774f34b641200617bfe1f89c929f1b6c3a4
Finlay Marno <finlay.marno@codeplay.com>
2025-03-11 10:03:03 +0000

    Keep the sycl::device alive by storing it in DeviceAllocation
    The removes the need for changes to examples

--- examples/48_hopper_warp_specialized_gemm/48_hopper_warp_specialized_gemm.cu
+++ examples/48_hopper_warp_specialized_gemm/48_hopper_warp_specialized_gemm.cu
@@ -338,14 +338,6 @@ void initialize(const Options &options) {
   initialize_block(block_C, seed + 2021);
 }
 
-void deinitialize() {
-  block_A.reset();
-  block_B.reset();
-  block_C.reset();
-  block_D.reset();
-  block_ref_D.reset();
-}
-
 /// Populates a Gemm::Arguments structure from the given commandline options
 typename Gemm::Arguments args_from_options(const Options &options)
 {
@@ -406,8 +398,6 @@ template <typename Gemm>
 int run(Options &options)
 {
   initialize(options);
-  using defer = std::shared_ptr<void>;
-  defer _(nullptr, [](auto){ deinitialize(); }); // avoid siof
 
   // Instantiate CUTLASS kernel depending on templates
   Gemm gemm;
